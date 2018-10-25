ddfbbf090d53c8b9b80e80f6572e55ed8d184c16
akaizhu003 <akaizhu003@163.com>
2018-10-25 17:41:11 +0800

    Update main.js

--- template/src/main.js
+++ template/src/main.js
@@ -8,9 +8,7 @@ import App from './App'
 import router from './router'
 {{/router}}
 {{#vuex}}  //vuex为true的时候就会写入这些
-import Vuex from 'vuex'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
 import store from  './store'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
-Vue.use(Vuex){{#if_eq lintConfig "airbnb"}};{{/if_eq}}
 {{/vuex}}
 {{#axios}}
 import Axios from 'axios'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
